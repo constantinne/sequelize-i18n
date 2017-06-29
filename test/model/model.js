6da462c715513df603db3d1204eac1bbcdab49e3
Cuciurca Constantin <constantincuciurca@gmail.com>
2017-06-29 16:14:32 +0200

    Pretify code

--- test/model/model.js
+++ test/model/model.js
@@ -1,24 +1,25 @@
-var Model = function (sequelize, DataTypes) {
-    var Model = sequelize.define('model', {
-        id : {
-            type 			: DataTypes.BIGINT,
-            primaryKey 		: true,
-            autoIncrement 	: true
-        },
-        label : {
-            type 			: DataTypes.STRING,
-            i18n		 	: true,
-        },
-        reference : {
-            type 			: DataTypes.STRING,
-        }
-    },
-    
-    { freezeTableName : true });
-    return Model;
+var Model = function(sequelize, DataTypes) {
+	var Model = sequelize.define(
+		'model',
+		{
+			id: {
+				type: DataTypes.BIGINT,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			label: {
+				type: DataTypes.STRING,
+				i18n: true
+			},
+			reference: {
+				type: DataTypes.STRING
+			}
+		},
+		{ freezeTableName: true }
+	);
+	return Model;
 };
 
-
 module.exports = function(sequelize) {
-    return sequelize.import('model', Model)
+	return sequelize.import('model', Model);
 };
